a37c249a479946ec91d194aba8700cf23d653855
Maksimg <m.gasanov@mafin.ru>
2023-09-05 17:21:42 +0400

    Настроен аллюр для запуска локально, мелкие настройки

--- src/test/java/tests/APITests.java
+++ src/test/java/tests/APITests.java
@@ -1,5 +1,8 @@
 package tests;
 
+import io.qameta.allure.Description;
+import io.qameta.allure.Step;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import utils.APIUtils;
 
@@ -11,36 +14,42 @@ public class APITests {
 
 
     @Test
-    public void testGetReq(){
+    public void testGetReq() {
         APIUtils.sendGetRequest(BASE_URL + "/api/companies/");
     }
+
     @Test
-    public void testPostReq(){
+    @DisplayName("post-запрос")
+    @Description("Тут описание теста")
+    @Step("Проверка отправки пост запроса")
+    public void testPostReq() {
         HashMap data = new HashMap();
         data.put("first_name", "Sam");
         data.put("last_name", "Watson");
         data.put("company_id", 3);
-        id = APIUtils.sendPostRequest(BASE_URL + "/api/users/", data )
+        id = APIUtils.sendPostRequest(BASE_URL + "/api/users/", data)
                 .then().statusCode(201).extract()
                 .jsonPath().getInt("user_id");
         System.out.println(id);
     }
+
     @Test
-    public void testPutReq(){
+    public void testPutReq() {
         HashMap data = new HashMap();
         data.put("first_name", "Samuel");
         data.put("last_name", "Jackson");
         data.put("company_id", 3);
-        id=20835;
-        String uri = BASE_URL + "/api/users/"+id;
-        APIUtils.sendPutRequest(uri,data)
+        id = 20835;
+        String uri = BASE_URL + "/api/users/" + id;
+        APIUtils.sendPutRequest(uri, data)
                 .then().statusCode(200);
         System.out.println(uri);
     }
+
     @Test
-    public void testDeleteReq(){
-        id=20835;
-        APIUtils.sendDeleteRequest(BASE_URL + "/api/users/"+id)
+    public void testDeleteReq() {
+        id = 20835;
+        APIUtils.sendDeleteRequest(BASE_URL + "/api/users/" + id)
                 .then().statusCode(202).log().all();
     }
 
